aaf5d39824024abaaa8a7da0af6ca247c437236e
Brian Dukes <bdukes@engagesoftware.com>
2020-10-02 13:25:57 -0500

    Fix StyleCop warnings

--- DNN Platform/DotNetNuke.Web/Common/WebFormsServiceProvider.cs
+++ DNN Platform/DotNetNuke.Web/Common/WebFormsServiceProvider.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
-
 namespace DotNetNuke.Web.Common
 {
     using System;
@@ -11,7 +10,7 @@ namespace DotNetNuke.Web.Common
     using DotNetNuke.Common;
     using DotNetNuke.Common.Extensions;
     using DotNetNuke.Instrumentation;
-    
+
     using Microsoft.Extensions.DependencyInjection;
 
     /// <summary>
@@ -19,39 +18,52 @@ namespace DotNetNuke.Web.Common
     /// </summary>
     internal class WebFormsServiceProvider : IServiceProvider
     {
-    	private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(WebFormsServiceProvider));
-    
         private const BindingFlags ActivatorFlags =
             BindingFlags.Instance |
             BindingFlags.NonPublic |
             BindingFlags.Public |
             BindingFlags.CreateInstance;
 
+        private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(WebFormsServiceProvider));
+
         /// <inheritdoc />
         public object GetService(Type serviceType)
         {
-        	bool hasHttpContext;
-        	bool hasScope;
+            bool hasHttpContext;
+            bool hasScope;
             IServiceProvider provider = null;
-            if (HttpContextSource.Current == null) {
-            	hasHttpContext = false;
-            	hasScope = false;
-            } else {
-            	hasHttpContext = true;
-            	var scope = HttpContextSource.Current.GetScope();
-            	if (scope == null) {
-            		hasScope = false;
-            	} else {
-            		hasScope = true;
-            		provider = scope.ServiceProvider;
-            	}
+            if (HttpContextSource.Current == null)
+            {
+                hasHttpContext = false;
+                hasScope = false;
             }
-            
+            else
+            {
+                hasHttpContext = true;
+                var scope = HttpContextSource.Current.GetScope();
+                if (scope == null)
+                {
+                    hasScope = false;
+                }
+                else
+                {
+                    hasScope = true;
+                    provider = scope.ServiceProvider;
+                }
+            }
+
             Logger.WarnFormat("Resolving {0} with {1} (context: {2}, scope: {3}, interface: {4}, constructors: {5})", serviceType, provider, hasHttpContext, hasScope, serviceType.IsInterface, serviceType.GetConstructors().Length);
 
             return provider != null && (serviceType.IsInterface || serviceType.GetConstructors().Length > 0)
                 ? ActivatorUtilities.GetServiceOrCreateInstance(provider, serviceType)
                 : Activator.CreateInstance(serviceType, ActivatorFlags, null, null, null);
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="WebFormsServiceProvider"/> class.</summary>
+        /// <param name="globalServiceProvider">The service provider to use to create scopes for each request.</param>
+        public WebFormsServiceProvider(IServiceProvider globalServiceProvider)
+        {
+            this.globalServiceProvider = globalServiceProvider;
         }
     }
 }
